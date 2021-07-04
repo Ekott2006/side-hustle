b21bf1e17fd0dd9b3b57e10356af37b7ef60dfe0
Ekott2006 <84778161+Ekott2006@users.noreply.github.com>
2021-07-04 20:26:27 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+// Home
+void main() {
+  runApp(MaterialApp(
+    title: 'Weather App',
+    home: Home(),
+  )
+  );
+}
+
+class Home extends StatefulWidget {
+  @override
+  State <StatefulWidget> createState () {
+    return Body();
+  }
+}
+
+// Body
+class Body extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      // Top bar
+      appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Nigerian Weather',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40.0
+              )
+          )
+      ),
+
+      backgroundColor: Colors.white70,
+
+      // Body
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 0.0),
+        child: ListView(
+          children: <Widget>[
+
+            ListTile(
+                leading: Icon(Icons.water),
+                title: Text('Moisture'),
+                trailing: Text('5200ml'),
+              ),
+
+            ListTile(
+                leading: Icon(Icons.ac_unit),
+                title: Text('Humidity'),
+                trailing: Text('65mmHg'),
+              ),
+
+            ListTile(
+              leading: Icon(Icons.thermostat),
+              title: Text('Temperature'),
+                trailing: Text('52\u00B0C'),
+            ),
+
+            ListTile(
+            leading: Icon(Icons.air),
+            title: Text('Air pressure'),
+            trailing: Text('52psi'),
+          ),
+
+            ListTile(
+                leading: Icon(Icons.wb_sunny),
+                title: Text('Weather '),
+                trailing: Text('Sunny'),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
